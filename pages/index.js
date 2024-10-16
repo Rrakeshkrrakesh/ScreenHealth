fd0e6954e0f1f0adc62340c9a9c80f498e1ba24f
Rakesh <rrakeshkrrakesh@gmail.com>
2024-10-17 04:13:38 +0530

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to Health Screening App</h1>
-      <p>Please enter your age and country to get recommendations!</p>
-    </div>
-  );
-}
