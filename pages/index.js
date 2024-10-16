b4ee0c5a9ce86534e0e2fbae9b23b76969b5bef0
Rakesh <rrakeshkrrakesh@gmail.com>
2024-10-17 04:16:02 +0530

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Health Screening App</h1>
+      <p>Please enter your age and country to get recommendations!</p>
+    </div>
+  );
+}
